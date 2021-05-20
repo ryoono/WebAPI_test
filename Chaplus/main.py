b717f618bfd7c95d5586c9fcc5ef0e569c6d2274
ryoono <ono@pluslab.org>
2021-05-20 22:38:15 +0900

    chaplus code update

--- Chaplus/main.py
+++ Chaplus/main.py
@@ -2,9 +2,25 @@
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+import requests
+import json
+
+word = input("なにか話しかけてみてください：")
+
+load_dotenv(verbose=True)
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-AP = os.environ.get("API_KEY") # 環境変数の値をAPに代入
-print(AP)
+# リクエストに必要なパラメーター
+headers = {'content-type':'text/json'}
+payload = {'utterance':word}
+
+# APIKEYの部分は自分のAPI鍵を代入してください
+url = 'https://www.chaplus.jp/v1/chat?apikey=' + os.environ.get("CHAPLUS_KEY")
+
+# APIを叩く
+res = requests.post(url=url, headers=headers, data=json.dumps(payload))
+
+# 最適と思われるレスポンスを抽出
+print( res.json()['bestResponse']['utterance'] )
