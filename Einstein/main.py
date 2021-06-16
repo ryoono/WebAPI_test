68e46b0ed2215955a7ec36ed9a22c73c37e67444
ryoono <ono@pluslab.org>
2021-06-16 23:34:35 +0900

    Image recognition start

--- Einstein/main.py
+++ Einstein/main.py
@@ -4,6 +4,10 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 
 import requests
+import json
+
+from PIL import Image
+import io
 
 # JWT用
 import jwt
@@ -19,7 +23,35 @@ def main():
     load_dotenv(dotenv_path)
 
     encoded_jwt = generate_OAuth_Token()
-    get_Access_Token( encoded_jwt )
+    oauth = get_Access_Token( encoded_jwt )
+
+    # file_path = "./zou.jpg"
+    # img = Image.open(file_path, mode='r')
+    # img_bytes = io.BytesIO()
+    # img.save(img_bytes, format='PNG')
+    # img_bytes = img_bytes.getvalue()
+    print(oauth)
+
+    headers = {
+        'Authorization': 'Bearer ' + oauth,
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'multipart/form-data'
+    }
+
+    files = {
+        'sampleLocation': 'https://einstein.ai/images/generalimage.jpg',
+        'modelId': 'GeneralImageClassifier'
+    }
+
+    print("")
+    print(headers)
+    print("")
+    print(files)
+    print("")
+
+    response = requests.post('https://api.einstein.ai/v2/vision/predict', headers=headers, files=files)
+
+    print(response.text)
 
 
 # OAuthトークンを取得するためのJWT作成
@@ -61,11 +93,16 @@ def get_Access_Token( encoded_jwt ):
 
     # ヘッダーとデータの組み立て
     headers = {'Content-type': 'application/x-www-form-urlencoded'}
-    data = 'grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion=' + str(encoded_jwt)
+    data = {
+        'grant_type'    : 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+        'assertion'     : str(encoded_jwt)
+    }
 
     # OAuthトークン取得申請
     response = requests.post('https://api.einstein.ai/v2/oauth2/token', headers=headers, data=data)
-    print(response.text)
+    res_json = json.loads(response.text)
+
+    return res_json['access_token']
 
 
 if __name__ == '__main__':
